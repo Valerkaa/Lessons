6b9d61989ee0d66f5f3f8a9b3ff788fde5749a5e
Valetnikov <mr.valentikov.74@gmail.com>
2023-03-04 17:39:35 +0200

    Lesson 3 - reforgered3)

--- js/main.js
+++ js/main.js
@@ -14,18 +14,37 @@
 // Lesson 3
 
 const form = document.getElementById( "myForm" );
-let numbers = document.getElementById("numbers")
+// let numbers = document.getElementById("numbers")
 form.addEventListener( "submit", function ( event ) {
   event.preventDefault();
   let name = document.getElementById("name").value
   let lastName = document.getElementById("lastName").value
-  let email = document.getElementById("email").value
-  alert(`Имя:${name} Фамилия:${lastName} Почта:${email}`)
+  let age = Number(document.getElementById("age").value);
+  let town = document.getElementById("town").value
+  function makeUser(name,lastName, age ,town) {
+    return {
+      name,
+      lastName,
+      town,
+      age
+    };
+  }
+  let user = makeUser( name, lastName, age, town);
+  let agree = confirm("Разрешаете показывать ваши данные?")
+  function showUser() {
+    alert(`Имя:${user.name} Фамилия:${user.lastName}  Возраст:${user.age}  Городо:${user.town}`)
+  }
+  function hideUser() {
+    alert("Пользователь не дал доступа");
+  }
+
+  agree ? showUser() : hideUser()
+
 } );
 
-let idk = prompt('Введи цифру', "123456");;
-let probel = [].map.call(idk, function(x) {
-  return x;
-}).join(' ');
-numbers.value = probel
+// let idk = prompt('Введи цифру', "123456");;
+// let probel = [].map.call(idk, function(x) {
+//   return x;
+// }).join(' ');
+// numbers.value = probel
 // Lesson 3 End
