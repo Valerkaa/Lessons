1b077453b6a0236028a9f19d92f3d3dbd62aef90
Valetnikov <mr.valentikov.74@gmail.com>
2023-03-04 11:48:28 +0200

    Lesson 3

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-// Lesson 2
+// // Lesson 2
+//
+//
+// let question = prompt('What\'s your name?', "Name");
+//
+// alert(`Hello, ${question} How are you? `);
+//
+//
+//
+//
+// // Lesson 2 End
 
 
-let question = prompt('What\'s your name?', "Name");
+// Lesson 3
 
-alert(`Hello, ${question} How are you? `);
+const form = document.getElementById( "myForm" );
 
-
-
-
-// Lesson 2 End
+form.addEventListener( "submit", function ( event ) {
+  event.preventDefault();
+  let name = document.getElementById("name").value
+  let lastName = document.getElementById("lastName").value
+  let email = document.getElementById("email").value
+  alert(name +" " + lastName + " " + email)
+} );
+// Lesson 3 End
