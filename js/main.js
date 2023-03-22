7082af56ff1eafe8b7cc4b22663b46e963aa96c4
Valetnikov <mr.valentikov.74@gmail.com>
2023-03-23 00:01:40 +0200

    Lesson 7

--- js/main.js
+++ js/main.js
@@ -186,86 +186,164 @@
 
 
 //Lesson 6
-
-let arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
-let sum = 0;
-let countPositivNum = 0;
-arr.forEach(function(num) {
-  if( num > 0 ) {
-    countPositivNum++;
-    sum += num;
-  };
-});
-console.log(` Сумма положительных чисел = ${sum} -> Количество положительных чисел ${countPositivNum}`);
-
-
-let minNum = arr [0];
-arr.forEach(function(num){
-  if(num < minNum){
-    minNum = num;
-  }
-});
-console.log(`Позиция минимального числа :${arr.indexOf(minNum)}, Минимальное число ${minNum}`);
-
-
-let maxNum = arr [0];
-arr.forEach(function(num){
-  if(num > maxNum){
-    maxNum = num;
-  }
-});
-console.log(`Позиция максимального числа :${arr.indexOf(maxNum)}, Максимальное число ${maxNum}`);
-
-
-
-let countNegativeNum = 0;
-arr.forEach(function(num){
-  if( num < 0 ){
-    countNegativeNum += 1
-  }
-});
-console.log(`Количество отрицательных чисел:${countNegativeNum}`);
-
-
-
-let nonPairedPositiveNum = 0;
-let sumNonPairedPositiveNum = 0;
-arr.forEach(function(num){
-  nonPairedPositiveNum > 0
-  if( num % 3 === 0 ){
-    nonPairedPositiveNum += 1
-    sumNonPairedPositiveNum += num
-  }
-});
-console.log(`Количество положительных не парных чисел:${nonPairedPositiveNum},сумма чисел:${sumNonPairedPositiveNum} `);
-
-
-let pairedPositiveNum = 0;
-let sumPairedPositiveNum = 0;
-arr.forEach(function(num){
-  pairedPositiveNum > 0
-  if( num % 2 === 0 ){
-    pairedPositiveNum   += 1
-    sumPairedPositiveNum += num;
-  }
-});
-console.log(`Количество парных положительных чисел:${pairedPositiveNum},сумма этих чисел :${sumPairedPositiveNum} `);
-
-
-let productOfNum = 1;
-arr.forEach(function(num) {
-  if( num > 0 ) {
-    productOfNum *= num
-  }
-})
-console.log(`Произведение положительных чисел:${productOfNum}`);
-
-let newArr = arr.map(function(num) {
-  if( num !== maxNum ) {
-   return num = 0
-  }else{
-    return num =  maxNum
-  }
-})
-console.log(`Все кроме максимального будут нули: ${newArr}`)
+//
+// let arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+// let sum = 0;
+// let countPositivNum = 0;
+// arr.forEach(function(num) {
+//   if( num > 0 ) {
+//     countPositivNum++;
+//     sum += num;
+//   };
+// });
+// console.log(` Сумма положительных чисел = ${sum} -> Количество положительных чисел ${countPositivNum}`);
+//
+//
+// let minNum = arr [0];
+// arr.forEach(function(num){
+//   if(num < minNum){
+//     minNum = num;
+//   }
+// });
+// console.log(`Позиция минимального числа :${arr.indexOf(minNum)}, Минимальное число ${minNum}`);
+//
+//
+// let maxNum = arr [0];
+// arr.forEach(function(num){
+//   if(num > maxNum){
+//     maxNum = num;
+//   }
+// });
+// console.log(`Позиция максимального числа :${arr.indexOf(maxNum)}, Максимальное число ${maxNum}`);
+//
+//
+//
+// let countNegativeNum = 0;
+// arr.forEach(function(num){
+//   if( num < 0 ){
+//     countNegativeNum += 1
+//   }
+// });
+// console.log(`Количество отрицательных чисел:${countNegativeNum}`);
+//
+//
+//
+// let nonPairedPositiveNum = 0;
+// let sumNonPairedPositiveNum = 0;
+// arr.forEach(function(num){
+//   nonPairedPositiveNum > 0
+//   if( num % 3 === 0 ){
+//     nonPairedPositiveNum += 1
+//     sumNonPairedPositiveNum += num
+//   }
+// });
+// console.log(`Количество положительных не парных чисел:${nonPairedPositiveNum},сумма чисел:${sumNonPairedPositiveNum} `);
+//
+//
+// let pairedPositiveNum = 0;
+// let sumPairedPositiveNum = 0;
+// arr.forEach(function(num){
+//   pairedPositiveNum > 0
+//   if( num % 2 === 0 ){
+//     pairedPositiveNum   += 1
+//     sumPairedPositiveNum += num;
+//   }
+// });
+// console.log(`Количество парных положительных чисел:${pairedPositiveNum},сумма этих чисел :${sumPairedPositiveNum} `);
+//
+//
+// let productOfNum = 1;
+// arr.forEach(function(num) {
+//   if( num > 0 ) {
+//     productOfNum *= num
+//   }
+// })
+// console.log(`Произведение положительных чисел:${productOfNum}`);
+//
+// let newArr = arr.map(function(num) {
+//   if( num !== maxNum ) {
+//    return num = 0
+//   }else{
+//     return num =  maxNum
+//   }
+// })
+// console.log(`Все кроме максимального будут нули: ${newArr}`)
 //Lesson 6 end
+//lesson 7
+
+
+let users = [
+  {
+    "index": 0,
+    "isActive": true,
+    "balance": "$2,226.60",
+    "name": "Eugenia Sawyer",
+    "gender": "female",
+    "phone": "+1 (840) 583-3207",
+    "address": "949 John Street, Rose, Puerto Rico, 1857"
+  },
+  {
+    "index": 1,
+    "isActive": true,
+    "balance": "$2,613.77",
+    "name": "Pauline Gallegos",
+    "gender": "female",
+    "phone": "+1 (985) 593-3328",
+    "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+  },
+  {
+    "index": 2,
+    "isActive": false,
+    "balance": "$3,976.41",
+    "name": "Middleton Chaney",
+    "gender": "male",
+    "phone": "+1 (995) 591-2478",
+    "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
+  },
+  {
+    "index": 3,
+    "isActive": true,
+    "balance": "$1,934.58",
+    "name": "Burns Poole",
+    "gender": "male",
+    "phone": "+1 (885) 559-3422",
+    "address": "730 Seba Avenue, Osage, Alabama, 6290"
+  },
+  {
+    "index": 4,
+    "isActive": true,
+    "balance": "$3,261.65",
+    "name": "Mcfadden Horne",
+    "gender": "male",
+    "phone": "+1 (942) 565-3988",
+    "address": "120 Scholes Street, Kirk, Michigan, 1018"
+  },
+  {
+    "index": 5,
+    "isActive": false,
+    "balance": "$1,790.56",
+    "name": "Suzette Lewis",
+    "gender": "female",
+    "phone": "+1 (837) 586-3283",
+    "address": "314 Dunne Place, Bawcomville, Guam, 9053"
+  }
+]
+
+let intBalance = 0;
+const phoneArr = [];
+let sumOfBalance = 0;
+function filterBalance(balance){
+  intBalance = parseFloat(balance.balance.replace(/[$,]/g, ''))
+  if ( intBalance > 2000){
+    sumOfBalance += intBalance
+    return true
+  }
+}
+let arrayByBalance = users.filter(filterBalance)
+arrayByBalance.forEach((item) => phoneArr.push(item.phone))
+
+
+console.log(` Номера телефонов богачей для скама : ${phoneArr}`)
+console.log(`Всего можно заскамить ${Math.round(sumOfBalance)}$`)
+
+//lesson 7 end
