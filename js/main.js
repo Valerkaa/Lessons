24f971ec111b514679606bcc4d7d9497b758c520
Valetnikov <mr.valentikov.74@gmail.com>
2023-03-29 20:56:28 +0300

    Lesson 8

--- js/main.js
+++ js/main.js
@@ -271,79 +271,130 @@
 //Lesson 6 end
 //lesson 7
 
-
-let users = [
-  {
-    "index": 0,
-    "isActive": true,
-    "balance": "$2,226.60",
-    "name": "Eugenia Sawyer",
-    "gender": "female",
-    "phone": "+1 (840) 583-3207",
-    "address": "949 John Street, Rose, Puerto Rico, 1857"
-  },
-  {
-    "index": 1,
-    "isActive": true,
-    "balance": "$2,613.77",
-    "name": "Pauline Gallegos",
-    "gender": "female",
-    "phone": "+1 (985) 593-3328",
-    "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
-  },
-  {
-    "index": 2,
-    "isActive": false,
-    "balance": "$3,976.41",
-    "name": "Middleton Chaney",
-    "gender": "male",
-    "phone": "+1 (995) 591-2478",
-    "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
-  },
-  {
-    "index": 3,
-    "isActive": true,
-    "balance": "$1,934.58",
-    "name": "Burns Poole",
-    "gender": "male",
-    "phone": "+1 (885) 559-3422",
-    "address": "730 Seba Avenue, Osage, Alabama, 6290"
-  },
-  {
-    "index": 4,
-    "isActive": true,
-    "balance": "$3,261.65",
-    "name": "Mcfadden Horne",
-    "gender": "male",
-    "phone": "+1 (942) 565-3988",
-    "address": "120 Scholes Street, Kirk, Michigan, 1018"
-  },
-  {
-    "index": 5,
-    "isActive": false,
-    "balance": "$1,790.56",
-    "name": "Suzette Lewis",
-    "gender": "female",
-    "phone": "+1 (837) 586-3283",
-    "address": "314 Dunne Place, Bawcomville, Guam, 9053"
-  }
-]
-
-let intBalance = 0;
-const phoneArr = [];
-let sumOfBalance = 0;
-function filterBalance(balance){
-  intBalance = parseFloat(balance.balance.replace(/[$,]/g, ''))
-  if ( intBalance > 2000){
-    sumOfBalance += intBalance
-    return true
-  }
-}
-let arrayByBalance = users.filter(filterBalance)
-arrayByBalance.forEach((item) => phoneArr.push(item.phone))
-
-
-console.log(` Номера телефонов богачей для скама : ${phoneArr}`)
-console.log(`Всего можно заскамить ${Math.round(sumOfBalance)}$`)
+//
+// let users = [
+//   {
+//     "index": 0,
+//     "isActive": true,
+//     "balance": "$2,226.60",
+//     "name": "Eugenia Sawyer",
+//     "gender": "female",
+//     "phone": "+1 (840) 583-3207",
+//     "address": "949 John Street, Rose, Puerto Rico, 1857"
+//   },
+//   {
+//     "index": 1,
+//     "isActive": true,
+//     "balance": "$2,613.77",
+//     "name": "Pauline Gallegos",
+//     "gender": "female",
+//     "phone": "+1 (985) 593-3328",
+//     "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
+//   },
+//   {
+//     "index": 2,
+//     "isActive": false,
+//     "balance": "$3,976.41",
+//     "name": "Middleton Chaney",
+//     "gender": "male",
+//     "phone": "+1 (995) 591-2478",
+//     "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
+//   },
+//   {
+//     "index": 3,
+//     "isActive": true,
+//     "balance": "$1,934.58",
+//     "name": "Burns Poole",
+//     "gender": "male",
+//     "phone": "+1 (885) 559-3422",
+//     "address": "730 Seba Avenue, Osage, Alabama, 6290"
+//   },
+//   {
+//     "index": 4,
+//     "isActive": true,
+//     "balance": "$3,261.65",
+//     "name": "Mcfadden Horne",
+//     "gender": "male",
+//     "phone": "+1 (942) 565-3988",
+//     "address": "120 Scholes Street, Kirk, Michigan, 1018"
+//   },
+//   {
+//     "index": 5,
+//     "isActive": false,
+//     "balance": "$1,790.56",
+//     "name": "Suzette Lewis",
+//     "gender": "female",
+//     "phone": "+1 (837) 586-3283",
+//     "address": "314 Dunne Place, Bawcomville, Guam, 9053"
+//   }
+// ]
+//
+// let intBalance = 0;
+// const phoneArr = [];
+// let sumOfBalance = 0;
+// function filterBalance(balance){
+//   intBalance = parseFloat(balance.balance.replace(/[$,]/g, ''))
+//   if ( intBalance > 2000){
+//     sumOfBalance += intBalance
+//     return true
+//   }
+// }
+// let arrayByBalance = users.filter(filterBalance)
+// arrayByBalance.forEach((item) => phoneArr.push(item.phone))
+//
+//
+// console.log(` Номера телефонов богачей для скама : ${phoneArr}`)
+// console.log(`Всего можно заскамить ${Math.round(sumOfBalance)}$`)
 
 //lesson 7 end
+
+
+
+//lesson 8
+
+
+
+
+
+let currElem = 0;
+const arr = [1, 2, 3, 4, 5, 6, 7];
+function deleteElem(arr,elem){
+  currElem = arr.indexOf(elem)
+  arr.splice(currElem, 1);
+  return arr
+}
+const chars =
+  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+ function generateCustomPassword(passwordLength,chars){
+  let password = "";
+  for (let i = 0; i <= passwordLength; i++) {
+    const randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+
+  return password;
+};
+
+
+
+function deleteSymbols(str, symbols) {
+  let arr = str.toLowerCase().split("").filter((item) => !symbols.includes(item)).join("");
+  return arr;
+}
+
+
+function sumOf (){
+   let ourSum = 0;
+   return function (x){
+     ourSum += x
+     return ourSum
+   }
+ }
+ let sum = sumOf();
+
+
+
+
+
+//lesson 8 end
